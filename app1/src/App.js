5ca8b92e32ebda9e4243c560b452a3254fc4d423
vivian <vivian@vivians-MacBook-Pro.local>
2019-02-05 12:17:16 -0700

    done

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constuctor(props) {
-
+  constructor() {
+    super()
     this.state = {
       myName: 'Joe'
     }
